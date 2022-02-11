374d69122f740d73dafd04988f006cb45171af23
Sam Forbyn <samforbyn@gmail.com>
2022-02-10 21:11:13 -0700

    added create users in schema, index.html

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
+from smtpd import DebuggingServer
 from flask import Flask, render_template, redirect, request
 from models import create_post
 
 app = Flask(__name__)
 
-app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def homepage():
-    if request.method == "GET":
-        pass
+    return render_template("index.html")
 
-    if request.method == "POST":
-        name = request.form.get("name")
-        post = request.form.get("post")        
-        create_post(name, post)
-
-    return render_template('index.html')
-
+if __name__ == '__main__':
+    app.run(debug=True, host="127.0.0.1")
--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
-DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS favorites;
+
+CREATE TABLE users (
+    user_id int SERIAL PRIMARY KEY,
+    first_name VARCHAR(256) NOT NULL,
+    last_name VARCHAR(256) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    username VARCHAR(256) NOT NULL UNIQUE,
+    password VARCHAR(256) NOT NULL
+);
