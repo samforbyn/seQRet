c9b517274275941fe5cdf2d6b9f1871b17828baa
Sam Forbyn <samforbyn@gmail.com>
2022-02-13 09:20:12 -0700

    added basic login/signup templates,+ respective @auth.routes

--- website/auth.py
+++ website/auth.py
@@ -5,12 +5,11 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
-
+    return render_template('login.html')
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>sign-up</p>"
+    return render_template('sign_up.html')
